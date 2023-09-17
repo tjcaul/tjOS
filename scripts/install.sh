49c47354028ec21db1bd07d44eec019869d570a4
Tyson Caul <tjcaul@icloud.com>
2023-09-16 20:09:29 -0400

    Generalize `install.sh` to any drive

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 cd "$(dirname $(realpath $0))/.."
 
+volume="${1:-/Volumes/No Name/EFI/}"
 scripts/mount.sh
-cp build/BOOTX64.EFI /Volumes/No\ Name/EFI/BOOT/
+cp build/BOOTX64.EFI $volume/BOOT/
 scripts/umount.sh
